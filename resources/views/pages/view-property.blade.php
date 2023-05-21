59ac5b376cea5b33f2dd5868d7f3843b7ea85493
WalobwaD <walobwadan@gmail.com>
2023-05-21 23:12:14 +0300

    Modified location

--- resources/views/pages/view-property.blade.php
+++ resources/views/pages/view-property.blade.php
@@ -41,7 +41,7 @@
 
                                 <div class="row">
                                     <div class="col-12">
-                                        <h5>Juja, Nairobi</h5>
+                                        <h5>{{ $data[0]['property']->name }}</h5>
                                     </div>
                                 </div>
 
