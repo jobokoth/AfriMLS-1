355818e7ac2e6d3cdc3ca40bfe5d85c84700845e
WalobwaD <walobwadan@gmail.com>
2023-05-21 10:47:23 +0000

    Change Location

--- resources/views/pages/view-property.blade.php
+++ resources/views/pages/view-property.blade.php
@@ -41,7 +41,7 @@
 
                                 <div class="row">
                                     <div class="col-12">
-                                        <h5>Karen, Nairobi</h5>
+                                        <h5>Juja, Nairobi</h5>
                                     </div>
                                 </div>
 
