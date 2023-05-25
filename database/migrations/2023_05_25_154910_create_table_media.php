47b7221fb224c34e242a2c23d16de560c6d4d008
WalobwaD <walobwadan@gmail.com>
2023-05-25 18:55:27 +0300

    Fixed migrations

--- database/migrations/2023_05_25_154910_create_table_media.php
+++ database/migrations/2023_05_25_154910_create_table_media.php
@@ -1,11 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateMediaTable extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('media', function (Blueprint $table) {
@@ -29,4 +34,14 @@ class CreateMediaTable extends Migration
             $table->nullableTimestamps();
         });
     }
-}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+};
